c3d7190618d3ded2cb14e113c7ae68f8d5f68f11
enzgamilla <laurenceangelogamilla@gmail.com>
2023-11-25 11:28:36 +0400

    add index ts to import direct on file only

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import "bootstrap/dist/css/bootstrap.css";
-// import ListGroup from "./components/ListGroup/ListGroup";
-import Button from "./components/Button/Button";
-import Alert from "./components/Alert/Alert";
+import ListGroup from "./components/ListGroup";
+import Button from "./components/Button";
+import Alert from "./components/Alert";
 import { useState } from "react";
 
 function App() {
-  // const listOfState = [
-  //   "Abu Dhabi",
-  //   "Dubai",
-  //   "Sharjah",
-  //   "Ajman",
-  //   "Umm Al Quwain",
-  //   "Ras Al Khaima",
-  //   "Fujairah",
-  //   "Al Ain",
-  // ];
+  const listOfState = [
+    "Abu Dhabi",
+    "Dubai",
+    "Sharjah",
+    "Ajman",
+    "Umm Al Quwain",
+    "Ras Al Khaima",
+    "Fujairah",
+    "Al Ain",
+  ];
 
   const [show, setShow] = useState(true);
 
@@ -24,7 +24,7 @@ function App() {
 
   return (
     <div className="container">
-      {/* <ListGroup listState={listOfState} /> */}
+      <ListGroup listState={listOfState} />
       {!show && <Alert onClick={() => setShow(handleClick())} />}
       <Button onClick={() => setShow(handleClick())}>Show Alert</Button>
     </div>

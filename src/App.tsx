5d761a2ee96a42e4c0aa331d05f0a9e46cda47c4
enzgamilla <laurenceangelogamilla@gmail.com>
2023-11-25 10:29:12 +0400

    add dynamic list

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,20 @@ import "bootstrap/dist/css/bootstrap.css";
 import ListGroup from "./components/ListGroup/ListGroup";
 
 function App() {
+  const listOfState = [
+    "Abu Dhabi",
+    "Dubai",
+    "Sharjah",
+    "Ajman",
+    "Umm Al Quwain",
+    "Ras Al Khaima",
+    "Fujairah",
+    "Al Ain",
+  ];
+
   return (
-    <div>
-      <ListGroup />
+    <div className="container">
+      <ListGroup listState={listOfState} />
     </div>
   );
 }
--- src/components/ListGroup/ListGroup.tsx
+++ src/components/ListGroup/ListGroup.tsx
@@ -1,11 +1,13 @@
-function ListGroup() {
+interface Props {
+  listState: string[];
+}
+
+function ListGroup({ listState }: Props) {
   return (
     <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
+      {listState.map((list) => (
+        <li className="list-group-item text-center">{list}</li>
+      ))}
     </ul>
   );
 }
